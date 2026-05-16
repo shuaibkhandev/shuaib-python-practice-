db56d9f5cbcabe6b87826b8aa8c47eded7c87247
shuaibkhan <skhan.csit@gmail.com>
2026-05-16 18:23:07 +0500

    work with strings

--- hello.py
+++ hello.py
@@ -1,1 +1,27 @@
 print("Welcome to Pyton3!!")
+
+
+# Numbers In Python
+
+# num1 = 4
+# num2 = 7
+# num3 = 10
+
+# print(num1 < num2 < num3)
+# print(num1 < num2 and num2 < num3)
+# print(2==2<3)
+# print(2==2 and 2<3)
+
+# import math
+
+# print(math.floor(3.9))
+# print(math.floor(-3.9))
+
+# print(math.trunc(3.9))
+# print(math.trunc(-3.9))
+
+
+# Strings In Python
+
+username = "Shuaib Khan"
+print(username[0:7])
